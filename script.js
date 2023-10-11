9d5c866497bd9d87844dcc4d5276c623708aef10
subhaishere <60627114+subhaishere@users.noreply.github.com>
2023-10-11 21:03:01 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,23 +1,37 @@
-// complete this js code
+// Constructor function for Person
 function Person(name, age) {
-	this.name  = name;
-	this.age  = age;
-}
-
-function Employee(name, age, jobTitle) {
-	this.name  = name;
+	this.name = name;
 	this.age = age;
+  }
+  
+  // Add a method to the Person prototype
+  Person.prototype.greet = function() {
+	console.log(`Hello, my name is ${this.name} and I am ${this.age} years old.`);
+  }
+  
+  // Constructor function for Employee
+  function Employee(name, age, jobTitle) {
+	// Call the Person constructor to inherit name and age properties
+	Person.call(this, name, age);
 	this.jobTitle = jobTitle;
-}
-
-Person.prototype.greet= function(){
-	console.log(`Hello, my name is ${this.name}, I am ${this.age} years old.`)
-	alert(`Hello, my name is ${this.name}, I am ${this.age} years old.`)
-}
-
-Employee.prototype.jobGreet = function(){
+  }
+  
+  // Inherit the Person prototype
+  Employee.prototype = Object.create(Person.prototype);
+  Employee.prototype.constructor = Employee;
+  
+  // Add a method to the Employee prototype
+  Employee.prototype.jobGreet = function() {
 	console.log(`Hello, my name is ${this.name}, I am ${this.age} years old, and my job title is ${this.jobTitle}.`);
-} 
-// Do not change code below this line
-window.Person = Person;
-window.Employee = Employee;
+  }
+  
+  // Example usage:
+  window.Person = Person;
+  window.Employee = Employee;
+  const person1 = new Person("Alice", 30);
+ // person1.greet(); 
+  
+  const employee1 = new Employee("Bob", 25, "Manager");
+  employee1.greet(); 
+  employee1.jobGreet(); 
+  
